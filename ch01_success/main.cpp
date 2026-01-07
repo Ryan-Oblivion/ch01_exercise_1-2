387b8055468abad6f2b325e0c5971ac9ac272fbd
Ryan Johnson <rjohnson@rockefeller.edu>
2026-01-07 13:53:08 -0500

    Initial Commit

---
+++ ch01_success/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  ch01_success
+//
+//  Created by ryan johnson on 1/7/26.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // insert code here...
+    
+    cout << "Success!" << endl ;
+    return 0 ;
+    
+    // cout << "Hello, World!\n";
+    // return EXIT_SUCCESS;
+}
